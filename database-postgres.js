809aecd5d59f8ce6276b30876ba9d21406712e32
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-12 19:54:23 -0300

    feat: get all videos from db implemented

--- database-postgres.js
+++ database-postgres.js
@@ -1,13 +1,20 @@
 import { randomUUID } from 'node:crypto'
+import { sql } from './db.js'
 
 export class DatabasePostgres {
   #videos = new Map()
 
-  list(search) {
+  async list(search) {
+    let videos
     
+    if (search) {
+      videos = await sql`select * from videos where title ilike "%${search}%"`
+    } else {
+      videos =  await sql`select * from videos`
+    }
   }
-  create(video) {
-  
+  async create(video) {
+  const videoId = randomUUID()
   }
   update(id, video) {
   }
